8e04a63671d99e2c3e53d0fd23a6b4fe29ac730c
raimoncoding <robertjanstefan@gmail.com>
2024-02-02 12:26:57 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// document.getElementById('contact-form').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent default form submission
+//
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var message = document.getElementById('message').value;
+//
+//     if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+//       alert('Please fill in all fields.');
+//       return;
+//     }
+//
+//     // Form data is valid, you can proceed with further actions like AJAX submission or other operations
+//
+//     document.getElementById('contact-form').reset();
+//   });
+//
